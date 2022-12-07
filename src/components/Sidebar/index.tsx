23f262a122ac8d88cde7fa975da8793175740021
Isaac Cidreira <isaaccidreira@hotmail.com>
2022-12-07 17:17:55 -0300

    finalizado

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,32 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from '../Avatar';
+
+import styles from './styles.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar
+          hasBorder
+          src="https://avatars.githubusercontent.com/u/80259818?s=400&u=f320efab966377494abc20a241927e2d78a416ea&v=4"
+        />
+
+        <strong>Gabriel Borges</strong>
+        <span>Full Stack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
